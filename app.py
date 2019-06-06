1a4357757020e96937bc7f0961e2c78422c5ebe0
bk2g18 <bk2g18@soton.ac.uk>
2019-06-06 10:27:36 +0100

    added in the layout and register forms

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '974ffa637177ef3fe7382d23f9fb0cb8d9529bc2803b6d72b10809349095e0ce'
+
 
 posts = [
     {
@@ -23,6 +26,28 @@ posts = [
 def home_page():
     return render_template('home.html', posts=posts)
 
+
 @app.route('/about')
 def about_page():
     return render_template('about.html', title="About Page")
+
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home_page'))
+        else:
+            flash('Please check email and password', 'danger')
+    return render_template('login.html', title="Log In", form=form)
+
+
+@app.route('/register', methods=["POST", "GET"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('home_page'))
+    return render_template('register.html', title="Register", form=form)
